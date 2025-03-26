ef98b1d876c1a350d81a3609ecb025effbf6ca09
YoruanOrta <yoruanorta50@gmail.com>
2025-03-26 14:23:46 -0400

    Remove unused variables in taskBlock function for improved clarity

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,5 +1,10 @@
 export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
+
+  if (trueOrFalse) {
+    // Variables inside this block are unused and can be removed
+  }
+
   return [task, task2];
 }

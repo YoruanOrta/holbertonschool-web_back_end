ced740764694af2551a3edcaf4dea5af67343aba
YoruanOrta <yoruanorta50@gmail.com>
2025-03-26 14:30:30 -0400

    Fix import statement in getFullBudget.js to remove file extension and update income references for consistency

--- ES6_basic/9-getFullBudget.js
+++ ES6_basic/9-getFullBudget.js
@@ -1,14 +1,14 @@
-import getBudgetObject from './7-getBudgetObject.js';
+import getBudgetObject from './7-getBudgetObject';
 
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
     getIncomeInDollars() {
-      return `$${income}`;
+      return `$${this.income}`;
     },
     getIncomeInEuros() {
-      return `${income} euros`;
+      return `${this.income} euros`;
     },
   };
 

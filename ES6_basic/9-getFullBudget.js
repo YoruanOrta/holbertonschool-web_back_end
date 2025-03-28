e15b56d5cf534aec13903af2953f948e87661d70
YoruanOrta <yoruanorta50@gmail.com>
2025-03-28 09:50:11 -0400

    Update getIncomeInDollars and getIncomeInEuros methods to use budget.income for accurate values

--- ES6_basic/9-getFullBudget.js
+++ ES6_basic/9-getFullBudget.js
@@ -4,8 +4,8 @@ export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
-    getIncomeInDollars: () => `$${income}`,
-    getIncomeInEuros: () => `${income} euros`,
+    getIncomeInDollars: () => `$${budget.income}`,
+    getIncomeInEuros: () => `${budget.income} euros`,
   };
 
   return fullBudget;

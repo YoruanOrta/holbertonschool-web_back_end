a90f4d1a18ef466b51cba66fb8ec8414ea727546
YoruanOrta <yoruanorta50@gmail.com>
2025-04-23 18:16:50 -0400

    Add module to calculate the sum of a list of floats with type annotations

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""This module contains a function that calculates the sum of a list."""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Returns the sum of a list of floats."""
+    return sum(input_list)

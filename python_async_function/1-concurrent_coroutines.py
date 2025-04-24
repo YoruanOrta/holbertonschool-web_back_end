1cc1c07c29acc46f8ac25bd9d480e48d87f2fb1a
YoruanOrta <yoruanorta50@gmail.com>
2025-04-24 18:22:52 -0400

    Add coroutine wait_n to spawn multiple tasks and return sorted delays

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,15 @@
+#!usr/bin/env python3
+"""Spawns `n` coroutines with max delay and returns sorted delays."""
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """Returns a list of delays from `n` coroutines with max delay."""
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    delays = []
+    for task in asyncio.as_completed(tasks):
+        delay = await task
+        delays.append(delay)
+    return delays

8d2ffc7bd902a53d4275d0acb276a22068efde72
YoruanOrta <yoruanorta50@gmail.com>
2025-04-30 10:33:07 -0400

    fix

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -4,5 +4,4 @@
 
 def index_range(page: int, page_size: int) -> tuple:
     """Calculate the start and end index for pagination."""
-    r = range((page - 1) * page_size, page * page_size)
-    return r.start, r.stop
+    return (page - 1) * page_size, page * page_size

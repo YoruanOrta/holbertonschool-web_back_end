50cdde42e91f91efc1fc7022e89ba98a54cf7f3d
YoruanOrta <yoruanorta50@gmail.com>
2025-05-01 19:29:36 -0400

    fix

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -1,13 +1,9 @@
+#!/usr/bin/env python3
+""" 9-insert_school """
+
+
 def insert_school(mongo_collection, **kwargs):
-    """
-    Inserts a new document in the collection based on kwargs.
-
-    Parameters:
-        mongo_collection: pymongo.collection.Collection - The MongoDB collection where the document will be inserted.
-        **kwargs: Arbitrary keyword arguments representing the fields and values of the new document.
-
-    Returns:
-        The _id of the newly inserted document.
-    """
-    result = mongo_collection.insert_one(kwargs)  # Insert the document
-    return result.inserted_id  # Return the inserted document's _id
+    """Inserts a new document in a collection based on kwargs"""
+    new_document = kwargs
+    result = mongo_collection.insert_one(new_document)
+    return result.inserted_id

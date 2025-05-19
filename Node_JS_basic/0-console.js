901b2054565b084fe9b63546f088a7ea4e2c7881
YoruanOrta <yoruanorta50@gmail.com>
2025-05-19 17:01:59 -0400

    fix

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,7 +1,7 @@
-// 0-console.js
-
-function displayMessage(message) {
+// Define the displayMessage function using ES6 syntax
+const displayMessage = (message) => {
     console.log(message);
-  }
+  };
   
+  // Export the displayMessage function
   module.exports = displayMessage;
